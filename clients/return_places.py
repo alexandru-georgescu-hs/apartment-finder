7155ecb45828a7ec69e0f76dc0641d73e29c5c01
Alexandru Georgescu <alexandru.georgescu@hootsuite.com>
2017-05-08 17:16:22 +0300

    added search_place_nearby_id

--- clients/return_places.py
+++ clients/return_places.py
@@ -1,22 +1,27 @@
 import googlemaps
-from constants import BUCHAREST
 
-gmaps = googlemaps.Client(key='AIzaSyBQQr8KkPsUE-QA4wEw_zlKa7oKNZVviQY')
 
 class ReturnPlaces():
-# Use Places api:
-# https://github.com/googlemaps/google-maps-services-python/blob/master/googlemaps/places.py
+    # Use Places api:
+    # https://github.com/googlemaps/google-maps-services-python/blob/master/googlemaps/places.py
+
+    gmaps = googlemaps.Client(key='AIzaSyBQQr8KkPsUE-QA4wEw_zlKa7oKNZVviQY')
+    def search_place_nearby_id(self, location, radius, type):
+        """
+        returns a list of dictionaries containing 'location' coordinates and the 'id' for a location
+        """
+        places = []
+        result = self.gmaps.places_nearby(location=location, radius=radius, type=type)
+
+        for item in result['results']:
+            places.append(item['place_id'])
+        return places
+
     def search_place_nearby(self, location, radius, type):
         """
         returns a list of dictionaries containing 'location' coordinates and the 'id' for a location
         """
         places = []
-        result = gmaps.places_nearby(location=location, radius=radius, type=type)
+        result = self.gmaps.places_nearby(location=location, radius=radius, type=type)
+        return result['results']
 
-        # print type(places['results'])
-        for item in result['results']:
-            # places.append({'location': item['geometry']['location'], 'id': item['id']})
-            places.append(item['place_id'])
-        return places
-
-# print search_place_nearby(query='herastrau', location=BUCHAREST, radius=2000, type='park')
--- get_places.py
+++ get_places.py
@@ -2,4 +2,7 @@ from constants import BUCHAREST
 from clients.return_places import ReturnPlaces
 
 client = ReturnPlaces()
+print client.search_place_nearby_id(BUCHAREST, 2000, 'park')
+
+print "========================="
 print client.search_place_nearby(BUCHAREST, 2000, 'park')
