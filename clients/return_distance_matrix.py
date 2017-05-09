2cafb3bc683d9b16787789de4b9bb4e64df290c8
andreaposadino <andrea.posadino@hootsuite.com>
2017-05-09 15:15:23 +0200

    Refactor returned dat from getAllModesMatrix

--- clients/return_distance_matrix.py
+++ clients/return_distance_matrix.py
@@ -16,9 +16,24 @@ class ReturnDistanceMatrix:
         for mode in DEFAULT_MODES:
             matrix[mode] = self.getMatrix(origin,destinations,mode)
 
+        destinations_matrix = []
+        counter = 0
+        for destination in destinations:
+            destinations_matrix_element = {}
+            destinations_matrix_element["name"] = destination
+            modes_elements = {}
+            for mode in DEFAULT_MODES:
+                modes_elements[mode] = {}
+                if matrix[mode]['status'] == 'OK' and matrix[mode]['rows'][0]['elements'][counter]['status'] == 'OK':
+
+                    modes_elements[mode]['duration'] = matrix[mode]['rows'][0]['elements'][counter]['duration']
+                    modes_elements[mode]['distance'] = matrix[mode]['rows'][0]['elements'][counter]['distance']
+            counter+=1
+            destinations_matrix_element['modes'] = modes_elements
+            destinations_matrix.append(destinations_matrix_element)
             # address, mode, distance, duration
 
-        return matrix
+        return destinations_matrix
 
     def getTheClosestPlace(self,origin,places):
         destinations = []
