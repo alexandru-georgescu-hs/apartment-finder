ebcde4ecb5e5389275640920c150acc63ee52a46
Alexandru Georgescu <alexandru.georgescu@hootsuite.com>
2017-05-09 11:54:53 +0300

    a

--- get_places.py
+++ get_places.py
@@ -4,9 +4,9 @@ from clients.return_places import ReturnPlaces
 client = ReturnPlaces()
 print client.search_place_nearby_id(SAMPLE_ORIGIN, 6000, 'park')
 
-#To query an exact address
+#To query an exact address for long/lat
 start_point = client.search_place_nearby_query('Iuliu Maniu 94-100')
 print start_point
 
-#nearby needs long and lat
-print client.search_place_nearby(start_point, radius=4000, type='park')
+#Search for parks nearby long/lat
+print client.search_place_nearby(start_point, radius=2000, type='park')
